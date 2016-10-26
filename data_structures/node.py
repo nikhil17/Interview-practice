73de65af854c645fe01aea082311c6d41b221c89
Nikhil Karajgikar <nikhilkarajgikar@gmail.com>
2016-10-26 12:11:18 +0530

    removed node.pyc

--- data_structures/node.py
+++ data_structures/node.py
@@ -8,11 +8,10 @@ class Node(object):
 	# Override object to string to be the node value
 	def __str__(self):
 		return str(self.value)
-	
+
+#Node with previous pointer for doubly linked list
 class Node2(Node):
 	def __init__(self, value = None):
 		self.prev = None
 		super(Node2, self).__init__(value)
 
-a = Node2(3)
-print a.prev
